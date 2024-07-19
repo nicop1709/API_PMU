e6ba19c7dfbca05419e6b5f415939dbb91c2efeb
nicopages <nicolas.pages@telaqua.com>
2024-07-19 14:53:26 +0200

    Update configurations

--- app/database.py
+++ app/database.py
@@ -1,8 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
 DATABASE_URL = "postgresql://pgadmin:changepassword@192.168.1.91:5433/iapmu?client_encoding=utf8"
+#DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
