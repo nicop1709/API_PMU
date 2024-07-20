ce74468a8965b83dcac0b0cb4f4f3d87ac935276
nicop1709 <nico.pages17@gmail.com>
2024-07-20 12:21:47 +0200

    debug

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ end_date = st.text_input('Date de fin (YYYY-MM-DD)')
 if st.button('Obtenir les données'):
     if table_name and start_date and end_date:
         try:
-            response = pd.read_json(f"http://176.181.170.72:5003/data?table_name={table_name}&start_date={start_date}&end_date={end_date}")
+            response = pd.read_json(f"http://localhost:5003/data?table_name={table_name}&start_date={start_date}&end_date={end_date}")
             st.write(response)
         except Exception as e:
             st.error(f"Erreur lors de l'appel à l'API: {e}")
